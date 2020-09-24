d14f076c49d3e88695b2ef4d24043a517a6c16c0
Smooch CI - Android <ops+smooch-ci-android@smooch.io>
2020-09-24 15:24:20 +0000

    Release 8.0.0

--- demo/app/src/main/java/io/smooch/demoapp/IntroductionFragment.java
+++ demo/app/src/main/java/io/smooch/demoapp/IntroductionFragment.java
@@ -3,7 +3,7 @@ package io.smooch.demoapp;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
